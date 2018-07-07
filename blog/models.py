c19e9cd2337846d772db469d03706e2012b3d6a1
aclstack <aclstack@gmail.com>
2018-07-07 18:07:15 +0800

    admin 后台界面显示修改

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,5 @@ class Article(models.Model):
     title = models.CharField(max_length=64, default='Title')
     content = models.TextField(null=True)
 
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from . import models
 
 
 def index(request):
-    article = models.Article.objects.get(pk=1)
+    article = models.Article.objects.get(pk=2)
     return render(request, 'blog/index.html', {'article': article})
