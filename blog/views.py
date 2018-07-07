50a10d2f7fb2fa72ef6f57385c922ead51a97bb3
aclstack <aclstack@gmail.com>
2018-07-07 18:18:38 +0800

    相关主页面修改

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from . import models
 
 
 def index(request):
-    article = models.Article.objects.get(pk=2)
-    return render(request, 'blog/index.html', {'article': article})
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
